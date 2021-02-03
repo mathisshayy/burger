0f9f4e0b3b540b3087b808d69a726a2fa632e053
mathisshayy <deshe.mathis@gmail.com>
2021-02-03 01:15:12 -0500

    orm

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ const orm = require("../config/orm");
 
 var burger = {
     selectAll: function(cb) {
-      orm.all("burger", function(res) {
+      orm.selectAll("burger", function(res) {
         cb(res);
       });
     },
 
     insertOne: function(cols, vals, cb) {
-      orm.create("burger", cols, vals, function(res) {
+      orm.insertOne("burger", cols, vals, function(res) {
         cb(res);
       });
     },
     updateOne: function(objColVals, condition, cb) {
-      orm.update("burger", objColVals, condition, function(res) {
+      orm.updateOne("burger", objColVals, condition, function(res) {
         cb(res);
       });
     },
