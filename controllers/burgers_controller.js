8d6dfa547ed3b086956b0476cadd988319d2ebc0
mathisshayy <deshe.mathis@gmail.com>
2021-02-03 00:00:23 -0500

    updates

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,62 @@
+const express = require("express");
+
+var burger = require("../models/burger");
+
+// router
+var router = express.Router();
+
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+      "burger_name", "devour"
+    ], [
+      req.body.burger_name, req.body.devour
+    ], function(result) {
+      
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    bureger.insertOne({
+      devour: req.body.devour
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "Double Cheese Burger", true);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "Chicken Sandwhich", true);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "Bacon Cheeseburger", false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (4, "Fish Sandwhich", true);
